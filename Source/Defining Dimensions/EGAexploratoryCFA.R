12fe75bf69047b8f32380af74d0edb558c2a7b1e
RiccardoImprota <riccardo.imp@gmail.com>
2023-08-25 20:16:34 +0200

    Implementing EGA clustering

---
+++ Source/Defining Dimensions/EGAexploratoryCFA.R
@@ -0,0 +1,52 @@
+# This code is not necessary to run any script.
+# It is only used to compute the cronbach alpha of the EGA dimensions.
+
+# load the necessary packages
+library(lavaan)
+library(readr)  # Data import
+library(psych)
+print(getwd())
+
+source("../Data Preparation/functions.R")
+
+df <-ImportReversedDF()
+
+EGAdimensions <-read_csv2("../../data/EGAdimensions.csv", show_col_types = FALSE)
+
+# Select the variables
+df <- df %>%
+  select(all_of(EGAdimensions$names))
+
+model_list <- list()
+for(dimension in unique(EGAdimensions$EGA_dimension)){
+  factor_variables <- EGAdimensions$names[EGAdimensions$EGA_dimension == dimension]
+  model_list[[dimension]] <- paste(dimension, " =~ ", paste(factor_variables, collapse = " + "), ";")
+}
+
+for(dimension in unique(EGAdimensions$EGA_dimension)){
+  factor_variables <- EGAdimensions$names[EGAdimensions$EGA_dimension == dimension]
+  if(length(factor_variables) == 2) {
+    # If there are only two observed variables, fix lambda
+    model_list[[dimension]] <- paste(dimension, " =~ lambda*", factor_variables[1], "+ lambda*", factor_variables[2], ";")
+  } else {
+    model_list[[dimension]] <- paste(dimension, " =~ ", paste(factor_variables, collapse = " + "), ";")
+  }
+}
+
+
+model_string <- paste(model_list, collapse = " ")
+
+cat(paste(model_list, collapse = "\n"))
+
+EGAdimensions$EGA_dimension %>% 
+  unique() %>% 
+  purrr::map_df(~list( dimension = .x,
+                       alpha = psych::alpha(df[, EGAdimensions$names[EGAdimensions$EGA_dimension == .x]],check.keys = TRUE)$total[["std.alpha"]]
+  ))
+
+
+# Fit the model
+fit <- cfa(model_string, data=df, estimator="WLSMV", missing = "pairwise",ordered=TRUE)
+
+summary(fit, fit.measures = TRUE, standardized = T)
+
---
+++ data/readme.txt
@@ -0,0 +1,5 @@
+In my local code repository, the subdirectories "source" and "processed" data are available within this directory. Due to legal constraints, I am unable to share this data on GitHub.
+
+Should the reader possess the appropriate dataset, it can be inserted in the .sav format into the data/source, and subsequently, a data/processed folder can be created.
+
+For the code to function correctly, the manually curated files named var_to_reverse.csv, used_variables.csv, and EGAdimensions.csv are also essential. Regrettably, I cannot yet share them due to legal restrictions.
